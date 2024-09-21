478f751badd105c87f3d60717d7c2f8afe0b9c34
Samuel <samadeolu7@gmail.com>
2024-09-21 16:03:27 +0100

    fixed allowed hosts

--- src/phoenix/settings.py
+++ src/phoenix/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yourdomain.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'krystartust-web.onrender.com','krystartust.ng']
 
 
 # Application definition
