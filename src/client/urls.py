2da6f8392cdc157d8bcd0fef28dcc66bd153c88c
Samuel <samadeolu7@gmail.com>
2024-11-28 11:19:34 +0100

    make search bars look better

--- src/client/urls.py
+++ src/client/urls.py
@@ -1,4 +1,4 @@
-from .views import create_client, edit_client, list_clients, individual_report, generate_client_id
+from .views import create_client, edit_client, list_clients, individual_report, generate_client_id_view
 
 from django.urls import path
 
@@ -7,5 +7,5 @@ urlpatterns = [
     path('edit/<int:client_id>/', edit_client, name='edit_client'),
     path('list/', list_clients, name='list_clients'),
     path('individual-report/<int:pk>/', individual_report, name='individual_report'),
-    path('generate-client-id/', generate_client_id, name='generate_client_id')
+    path('generate-client-id/', generate_client_id_view, name='generate_client_id')
 ]
